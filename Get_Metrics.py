ea5c5b5411450a90fb4630792f4d000dd76883ca
Rama Koteswara Rao Kandimalla <80211924@Ramas-MacBook-Pro.local>
2020-05-05 20:58:40 +0530

    Adding script for caliculating mAP and other metrics

---
+++ Get_Metrics.py
@@ -0,0 +1,334 @@
+import glob
+import json
+import os
+import shutil
+import operator
+import sys
+import argparse
+import math
+from statistics import mean 
+
+import numpy as np
+
+MINOVERLAP = 0.5 # default value (defined in the PASCAL VOC2012 challenge)
+
+specific_iou_flagged = False
+
+# make sure that the cwd() is the location of the python script (so that every path makes sense)
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+GT_PATH = os.path.join(os.getcwd(), 'input', 'ground-truth')
+DR_PATH = os.path.join(os.getcwd(), 'input', 'detection-results')
+
+
+"""
+ throw error and exit
+"""
+def error(msg):
+    print(msg)
+    sys.exit(0)
+
+"""
+ check if the number is a float between 0.0 and 1.0
+"""
+def is_float_between_0_and_1(value):
+    try:
+        val = float(value)
+        if val > 0.0 and val < 1.0:
+            return True
+        else:
+            return False
+    except ValueError:
+        return False
+
+"""
+ Calculate the AP given the recall and precision array
+    1st) We compute a version of the measured precision/recall curve with
+         precision monotonically decreasing
+    2nd) We compute the AP as the area under this curve by numerical integration.
+"""
+def voc_ap(rec, prec):
+    """
+    --- Official matlab code VOC2012---
+    mrec=[0 ; rec ; 1];
+    mpre=[0 ; prec ; 0];
+    for i=numel(mpre)-1:-1:1
+            mpre(i)=max(mpre(i),mpre(i+1));
+    end
+    i=find(mrec(2:end)~=mrec(1:end-1))+1;
+    ap=sum((mrec(i)-mrec(i-1)).*mpre(i));
+    """
+    rec.insert(0, 0.0) # insert 0.0 at begining of list
+    rec.append(1.0) # insert 1.0 at end of list
+    mrec = rec[:]
+    prec.insert(0, 0.0) # insert 0.0 at begining of list
+    prec.append(0.0) # insert 0.0 at end of list
+    mpre = prec[:]
+    """
+     This part makes the precision monotonically decreasing
+        (goes from the end to the beginning)
+        matlab: for i=numel(mpre)-1:-1:1
+                    mpre(i)=max(mpre(i),mpre(i+1));
+    """
+    # matlab indexes start in 1 but python in 0, so I have to do:
+    #     range(start=(len(mpre) - 2), end=0, step=-1)
+    # also the python function range excludes the end, resulting in:
+    #     range(start=(len(mpre) - 2), end=-1, step=-1)
+    for i in range(len(mpre)-2, -1, -1):
+        mpre[i] = max(mpre[i], mpre[i+1])
+    """
+     This part creates a list of indexes where the recall changes
+        matlab: i=find(mrec(2:end)~=mrec(1:end-1))+1;
+    """
+    i_list = []
+    for i in range(1, len(mrec)):
+        if mrec[i] != mrec[i-1]:
+            i_list.append(i) # if it was matlab would be i + 1
+    """
+     The Average Precision (AP) is the area under the curve
+        (numerical integration)
+        matlab: ap=sum((mrec(i)-mrec(i-1)).*mpre(i));
+    """
+    ap = 0.0
+    for i in i_list:
+        ap += ((mrec[i]-mrec[i-1])*mpre[i])
+    return ap, mrec, mpre
+
+
+"""
+ Convert the lines of a file to a list
+"""
+def file_lines_to_list(path):
+    # open txt file lines to a list
+    with open(path) as f:
+        content = f.readlines()
+    # remove whitespace characters like `\n` at the end of each line
+    content = [x.strip() for x in content]
+    return content
+
+"""
+ Create a ".temp_files/" and "output/" directory
+"""
+TEMP_FILES_PATH = ".temp_files"
+if not os.path.exists(TEMP_FILES_PATH): # if it doesn't exist already
+    os.makedirs(TEMP_FILES_PATH)
+
+"""
+ ground-truth
+     Load each of the ground-truth files into a temporary ".json" file.
+     Create a list of all the class names present in the ground-truth (gt_classes).
+"""
+# get a list with the ground-truth files
+ground_truth_files_list = glob.glob(GT_PATH + '/*.txt')
+if len(ground_truth_files_list) == 0:
+    error("Error: No ground-truth files found!")
+ground_truth_files_list.sort()
+# dictionary with counter per class
+gt_counter_per_class = {}
+counter_images_per_class = {}
+
+gt_files = []
+for txt_file in ground_truth_files_list:
+    #print(txt_file)
+    file_id = txt_file.split(".txt", 1)[0]
+    file_id = os.path.basename(os.path.normpath(file_id))
+    # check if there is a correspondent detection-results file
+    temp_path = os.path.join(DR_PATH, (file_id + ".txt"))
+
+    lines_list = file_lines_to_list(txt_file)
+    # create ground-truth dictionary
+    bounding_boxes = []
+    is_difficult = False
+    already_seen_classes = []
+    for line in lines_list:
+        try:
+            #print(line.split())
+            class_name, left, top, right, bottom = line.split()
+        except ValueError:
+            error_msg = "Error: File " + txt_file + " in the wrong format.\n"
+            error(error_msg)
+
+        bbox = left + " " + top + " " + right + " " +bottom
+
+        bounding_boxes.append({"class_name":class_name, "bbox":bbox, "used":False})
+        # count that object
+        if class_name in gt_counter_per_class:
+            gt_counter_per_class[class_name] += 1
+        else:
+            # if class didn't exist yet
+            gt_counter_per_class[class_name] = 1
+
+        if class_name not in already_seen_classes:
+            if class_name in counter_images_per_class:
+                counter_images_per_class[class_name] += 1
+            else:
+                # if class didn't exist yet
+                counter_images_per_class[class_name] = 1
+            already_seen_classes.append(class_name)
+
+
+    # dump bounding_boxes into a ".json" file
+    new_temp_file = TEMP_FILES_PATH + "/" + file_id + "_ground_truth.json"
+    gt_files.append(new_temp_file)
+    with open(new_temp_file, 'w') as outfile:
+        json.dump(bounding_boxes, outfile)
+
+gt_classes = list(gt_counter_per_class.keys())
+# let's sort the classes alphabetically
+gt_classes = sorted(gt_classes)
+n_classes = len(gt_classes)
+#print(gt_classes)
+#print(gt_counter_per_class)
+
+
+"""
+ detection-results
+     Load each of the detection-results files into a temporary ".json" file.
+"""
+# get a list with the detection-results files
+dr_files_list = glob.glob(DR_PATH + '/*.txt')
+dr_files_list.sort()
+
+for class_index, class_name in enumerate(gt_classes):
+    bounding_boxes = []
+    for txt_file in dr_files_list:
+        #print(txt_file)
+        # the first time it checks if all the corresponding ground-truth files exist
+        file_id = txt_file.split(".txt",1)[0]
+        file_id = os.path.basename(os.path.normpath(file_id))
+        temp_path = os.path.join(GT_PATH, (file_id + ".txt"))
+
+        lines = file_lines_to_list(txt_file)
+        for line in lines:
+            try:
+                tmp_class_name, confidence, left, top, right, bottom = line.split()
+            except ValueError:
+                error_msg = "Error: File " + txt_file + " in the wrong format.\n"
+                error(error_msg)
+            if tmp_class_name == class_name:
+                #print("match")
+                bbox = left + " " + top + " " + right + " " +bottom
+                bounding_boxes.append({"confidence":confidence, "file_id":file_id, "bbox":bbox})
+                #print(bounding_boxes)
+    # sort detection-results by decreasing confidence
+    bounding_boxes.sort(key=lambda x:float(x['confidence']), reverse=True)
+    with open(TEMP_FILES_PATH + "/" + class_name + "_dr.json", 'w') as outfile:
+        json.dump(bounding_boxes, outfile)
+
+"""
+ Calculate the AP for each class
+"""
+sum_AP = 0.0
+ap_dictionary = {}
+lamr_dictionary = {}
+# open file to store the output
+#with open(output_files_path + "/output.txt", 'w') as output_file:
+#output_file.write("# AP and precision/recall per class\n")
+count_true_positives = {}
+for class_index, class_name in enumerate(gt_classes):
+    count_true_positives[class_name] = 0
+    """
+        Load detection-results of that class
+    """
+    dr_file = TEMP_FILES_PATH + "/" + class_name + "_dr.json"
+    dr_data = json.load(open(dr_file))
+
+    """
+        Assign detection-results to ground-truth objects
+    """
+    nd = len(dr_data)
+    tp = [0] * nd # creates an array of zeros of size nd
+    fp = [0] * nd
+    for idx, detection in enumerate(dr_data):
+        file_id = detection["file_id"]
+        
+        # assign detection-results to ground truth object if any
+        # open ground-truth with that file_id
+        gt_file = TEMP_FILES_PATH + "/" + file_id + "_ground_truth.json"
+        ground_truth_data = json.load(open(gt_file))
+        ovmax = -1
+        gt_match = -1
+        # load detected object bounding-box
+        bb = [ float(x) for x in detection["bbox"].split() ]
+        for obj in ground_truth_data:
+            # look for a class_name match
+            if obj["class_name"] == class_name:
+                bbgt = [ float(x) for x in obj["bbox"].split() ]
+                bi = [max(bb[0],bbgt[0]), max(bb[1],bbgt[1]), min(bb[2],bbgt[2]), min(bb[3],bbgt[3])]
+                iw = bi[2] - bi[0] + 1
+                ih = bi[3] - bi[1] + 1
+                if iw > 0 and ih > 0:
+                    # compute overlap (IoU) = area of intersection / area of union
+                    ua = (bb[2] - bb[0] + 1) * (bb[3] - bb[1] + 1) + (bbgt[2] - bbgt[0]
+                                    + 1) * (bbgt[3] - bbgt[1] + 1) - iw * ih
+                    ov = iw * ih / ua
+                    if ov > ovmax:
+                        ovmax = ov
+                        gt_match = obj
+        # set minimum overlap
+        min_overlap = MINOVERLAP
+
+        if ovmax >= min_overlap:
+            if "difficult" not in gt_match:
+                    if not bool(gt_match["used"]):
+                        # true positive
+                        tp[idx] = 1
+                        gt_match["used"] = True
+                        count_true_positives[class_name] += 1
+                        # update the ".json" file
+                        with open(gt_file, 'w') as f:
+                                f.write(json.dumps(ground_truth_data))
+                        
+                    else:
+                        # false positive (multiple detection)
+                        fp[idx] = 1
+                        if show_animation:
+                            status = "REPEATED MATCH!"
+        else:
+            # false positive
+            fp[idx] = 1
+            if ovmax > 0:
+                status = "INSUFFICIENT OVERLAP"
+
+        
+
+    #print(tp)
+    # compute precision/recall
+    cumsum = 0
+    for idx, val in enumerate(fp):
+        fp[idx] += cumsum
+        cumsum += val
+    cumsum = 0
+    for idx, val in enumerate(tp):
+        tp[idx] += cumsum
+        cumsum += val
+    #print(tp)
+    rec = tp[:]
+    for idx, val in enumerate(tp):
+        rec[idx] = float(tp[idx]) / gt_counter_per_class[class_name]
+    
+    prec = tp[:]
+    for idx, val in enumerate(tp):
+        prec[idx] = float(tp[idx]) / (fp[idx] + tp[idx])
+
+    ap, mrec, mprec = voc_ap(rec[:], prec[:])
+    sum_AP += ap
+    #output_file.write(text + "\n Precision: " + str(rounded_prec) + "\n Recall :" + str(rounded_rec) + "\n\n")
+
+    ap_dictionary[class_name] = ap
+        
+
+    #output_file.write("\n# mAP of all classes\n")
+    #mAP = sum_AP / n_classes
+    metrics={}
+    metrics['mAP']=float(sum_AP / n_classes)
+    metrics['precision']=float(mean(prec))
+    metrics['recall']=float(mean(rec))
+
+    with open('metrics.json', 'w') as fp:
+        json.dump(metrics, fp)
+
+    print(metrics)
+
+# remove the temp_files directory
+shutil.rmtree(TEMP_FILES_PATH)
